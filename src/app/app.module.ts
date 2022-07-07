170857f94bdedae353722a44d1bb99511ba7e2c0
Jorge Rivera <jorge-421@hotmail.com>
2022-07-06 23:00:49 -0600

    OM-7 :package: agregando fortawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { HomeComponent, LoginComponent } from './pages';
+import { ButtonComponent } from './components/';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginRegisterFormLayoutComponent } from './layouts/login-register-form-layout/login-register-form-layout.component';
+import { TextInputComponent } from './components/text-input/text-input.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, LoginComponent, ButtonComponent, LoginRegisterFormLayoutComponent, TextInputComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
