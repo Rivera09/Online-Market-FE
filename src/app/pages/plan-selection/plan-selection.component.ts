f2151c007cb17a1ce46b7e3e5470e5ffe4d958b4
Luis Castillo <luigiernest999@gmail.com>
2022-07-09 15:28:14 -0600

    OM-5 :art: arreglo de llenado de plan

--- src/app/pages/plan-selection/plan-selection.component.ts
+++ src/app/pages/plan-selection/plan-selection.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanSelectionComponent implements OnInit {
 
+  plan = {
+    name : "Lite",
+    icon: "eco",
+    slogan: "Perfecto para comenzar",
+    type: "Gratis",
+    include:[
+      {
+        icon:"group",
+        content:"Crea hasta 5 Usuarios"
+      },
+      {
+        icon:"archive",
+        content:"Ten 500 Mb de uso"
+      },
+      {
+        icon:"article",
+        content:"Crea hasta 2 paginas"
+      },
+      {
+        icon:"shopping_bag",
+        content:"Adiciona 30 productos"
+      }
+    ]
+
+  }
+
+  plans = [
+    this.plan,
+    this.plan,
+    this.plan
+  ]
   constructor() { }
 
   ngOnInit(): void {
