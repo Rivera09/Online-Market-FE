4ec58a6e89b9571b1d038d22341a65c3276843ae
Jorge Rivera <jorge-421@hotmail.com>
2022-07-14 03:16:11 -0600

    OM-17 :lipstick: mejorando ui de layout principal

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-// import { SIDEBAR_OPTIONS } from 'src/contants';
+import { Component, OnInit, Input } from '@angular/core';
+import { SIDEBAR_OPTIONS, USER_ROLES } from 'src/contants';
+
+type TUserRoles = keyof typeof USER_ROLES;
+
+const titles: Record<TUserRoles, string> = {
+  ADMIN: 'Admin',
+};
 
 @Component({
   selector: 'app-sidebar',
@@ -7,12 +13,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  @Input() role: TUserRoles = 'ADMIN';
+  title = titles[this.role];
+
   constructor() {}
   /**
    * Una vez conectado con la API,
    * se debe filtrar con el role del usuario
    */
-  // options = SIDEBAR_OPTIONS.filter((option) => option.roles.includes('admin'));
+  options = SIDEBAR_OPTIONS.filter((option) =>
+    option.roles.includes(USER_ROLES[this.role])
+  );
 
   ngOnInit(): void {}
 }
