23d122b6c5ea789ce61f6737f54da0d683b64d51
Jorge Rivera <jorge-421@hotmail.com>
2022-07-15 23:50:33 -0600

    OM-11 :sparkles: creando pagina de bienvenida para admin

--- src/contants/index.ts
+++ src/contants/index.ts
@@ -1,10 +1,5 @@
 export { default as ROUTES } from './routes';
 export { default as USER_ROLES } from './roles';
 export { default as SIDEBAR_OPTIONS } from './sideBarOptions';
-export {
-  LOGO_PATHS,
-  LOGIN_IMG,
-  RECOVER_PASSWORD,
-  AUTH_IMAGES,
-} from './imagesPath';
-export { shopping  } from './interfaces'
+export * from './imagesPath';
+export * from './interfaces';
